e0d13ca0af9b992e1cb7e0411cfd19dcfa6118d9
DLR2204 <n130488@rguktn.ac.in>
2024-10-11 17:00:59 +0530

    Add files via upload

---
+++ ClassExamples/javascript/check.js
@@ -0,0 +1,8 @@
+function check(){
+
+	window.alert("Hello Everyone....Iam from window alert function");
+
+	document.write("Iam from document write function");
+	
+	console.log("Iam from cosole log function");
+}
